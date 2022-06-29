0c05a175f1201a3982690feb5cc3302c02d8db8e
matsubara-19082 <sd19082@tti-j.net>
2022-06-29 11:07:31 +0900

    日にちは固定，学籍番号は固定してない

--- library/views.py
+++ library/views.py
@@ -34,69 +34,17 @@ class IndexView(generic.ListView):
     model = BookList
     template_name = "library/book_list.html"
 
-    def get(self, request, *args, **kwargs):
-        # *args:複数の引数をタプルとして受け取る
-        # **kwargs:複数のキーワード引数を辞書型オブジェクトとして受け取る
-        user = self.request.user
-        print(user)
-
-        return super().get(request, **kwargs)
-
-    # def get(self, request, **kwargs):
-    #     print("呼ばれた")
-    #     print(request.method)
-    #     ctx = {
-    #         'user': self.request.user
-    #     }
-    #     print(ctx)
-    #     return self.render_to_response(ctx)
-        # FormのGETリクエストのパラメーターは以下のようにして取得できる。
-        # all_info = request.GET
-        # print(all_info)
-
-# def add(request):
-#     model = BookList
-#     form = BookListCreateForm(initial={
-#             'studentid': model.studentid,
-#         })
-#     context = { 'form': form }
-#     return render(request, 'library/book_add.html', context)
 
 class AddView(generic.CreateView):
     success_url = reverse_lazy("library:main")
     model = BookList
-    template_name = "library/book_add.html"
+    #template_name = "library/book_add.html"
     success_url = reverse_lazy("library:main")
     form_class = BookListCreateForm
-    # def get(self, request, **kwargs):
-    #     initial_dict = {"studentid": self.request.user}
-    #     form = BookListCreateForm(request.GET or None, initial=initial_dict)
-    #     return render(request, "library/book_add.html", dict(dorm=form))
-    #     # return super().get(request, **kwargs)
-
-    # def get(self, request, *args, **kwargs):
-            # model = BookList
-        # template_name = "library/book_add.html"
-        # print(self.request.use)
-        # form_class = BookListCreateForm(initial=initial_dict)
-        # ctx = {"form": form_class}
-        # return render(request, template_name, ctx)
-
-        # form_class = BookListCreateForm  # TODO 初期値設定
-    # def get(self, request):
-    #     initial_dict = dict(studentid="takuma")
-
-    #     return render(request, "library/book_add.html", dict(form=form))
-    # def get(self, request, *args, **kwargs):
-    #     # *args:複数の引数をタプルとして受け取る
-    #     # **kwargs:複数のキーワード引数を辞書型オブジェクトとして受け取る
-    #     # FormのGETリクエストのパラメーターは以下のようにして取得できる。
-    #     name = self.request.GET.get("username")
-    #     initial_dict = dict(studentid=name)
-    #     form_class = BookListCreateForm(initial=initial_dict)
-
-    #     return super().get(request, **kwargs)
-
+    def get(self, request, **kwargs):
+        initial_dict = {"studentid": self.request.user}
+        form = BookListCreateForm(request.GET or None, initial=initial_dict)
+        return render(request, "library/book_add.html", dict(form=form))
 
 class ReturnView(generic.DeleteView):
     model = BookList
